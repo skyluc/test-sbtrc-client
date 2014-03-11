4e010c5e128600955a81641ef2ce0d9e6ee96b46
Luc Bourlier <luc.bourlier@typesafe.com>
2014-03-11 16:24:16 +0100

    Test implementation of SbtClient using RxScala
    One wrapper provide a base API using Observable.
    The second wrapper adds a cache and a more simpler API.

--- src/main/scala/org/skyluc/sbtrc/SbtClientWithObservable.scala
+++ src/main/scala/org/skyluc/sbtrc/SbtClientWithObservable.scala
@@ -2,27 +2,80 @@ package org.skyluc.sbtrc
 
 import sbt.client.SbtClient
 import sbt.client.TaskResult
+import rx.lang.scala.Observable
+import sbt.protocol.MinimalBuildStructure
+import rx.lang.scala.Observer
+import rx.lang.scala.Subscriber
+import scala.concurrent.ExecutionContext.Implicits.global
+import rx.lang.scala.Subscription
+import rx.operators.OperationMulticast
+import rx.operators.OperationReplay
+import scala.concurrent.Future
+import scala.concurrent.Promise
+import sbt.client.TaskKey
+import sbt.protocol.ScopedKey
+import sbt.protocol.TaskSuccess
+import sbt.protocol.TaskFailure
+import sbt.client.SettingKey
 
-class SbtClientWithObservable extends SbtClient {
-  
+class SbtClientWithObservable(client: SbtClient) {
+
   // Members declared in java.io.Closeable
-  
-  def close(): Unit = ???
-  
-  // Members declared in sbt.client.SbtClient
-  
-  def handleEvents(listener: sbt.client.Event => Unit)(implicit ex: scala.concurrent.ExecutionContext): sbt.client.Subscription = ???
-  
-  def lookupScopedKey(name: String): scala.concurrent.Future[Seq[sbt.client.ScopedKey]] = ???
-  
-  def possibleAutocompletions(partialCommand: String,detailLevel: Int): scala.concurrent.Future[Set[sbt.client.Completion]] = ???
-  
-  def requestExecution(commandOrTask: String,interaction: Option[(sbt.client.Interaction, scala.concurrent.ExecutionContext)]):scala.concurrent.Future[Unit] = ???
-  
-  def watch[T](key: sbt.client.TaskKey[T])(l: (sbt.client.ScopedKey, sbt.client.TaskResult[T]) => Unit)(implicit ex: scala.concurrent.ExecutionContext): sbt.client.Subscription = ???
-  
-  def watch[T](key: sbt.client.SettingKey[T])(listener: (sbt.client.ScopedKey, sbt.client.TaskResult[T]) => Unit)(implicit ex: scala.concurrent.ExecutionContext): sbt.client.Subscription = ???
-  
-  def watchBuild(listener: sbt.client.MinimalBuildStructure => Unit)(implicit ex: scala.concurrent.ExecutionContext): sbt.client.Subscription = ???
 
+  def close(): Unit = {
+    // TODO: close all observables
+    client.close()
+  }
+
+  def lookupScopedKey(name: String): scala.concurrent.Future[Seq[sbt.client.ScopedKey]] = {
+    client.lookupScopedKey(name)
+  }
+
+  // Members declared in sbt.client.SbtClient
+
+//  def handleEvents(listener: sbt.client.Event => Unit)(implicit ex: scala.concurrent.ExecutionContext): sbt.client.Subscription = ???
+//
+//  def possibleAutocompletions(partialCommand: String, detailLevel: Int): scala.concurrent.Future[Set[sbt.client.Completion]] = ???
+//
+//  def requestExecution(commandOrTask: String, interaction: Option[(sbt.client.Interaction, scala.concurrent.ExecutionContext)]): scala.concurrent.Future[Unit] = ???
+
+  //------------
+
+  def buildWatcher(): Observable[MinimalBuildStructure] = {
+    val observable = Observable { subscriber: Subscriber[MinimalBuildStructure] =>
+      val subscription = client.watchBuild { b: MinimalBuildStructure =>
+        subscriber.onNext(b)
+      }
+      subscriber.add(Subscription {
+        subscription.cancel()
+      })
+    }
+
+    observable
+  }
+  
+  def keyWatcher[T](key: TaskKey[T]): Observable[(ScopedKey, TaskResult[T])] = {
+    val observable = Observable { subscriber: Subscriber[(ScopedKey, TaskResult[T])] =>
+      val subscription = client.watch(key) { (scopedKey, result) =>
+         subscriber.onNext((scopedKey, result))
+      }
+      subscriber.add(Subscription {
+        subscription.cancel
+      })
+    }
+    observable
+  }
+
+  def keyWatcher[T](key: SettingKey[T]): Observable[(ScopedKey, TaskResult[T])] = {
+    val observable = Observable { subscriber: Subscriber[(ScopedKey, TaskResult[T])] =>
+      val subscription = client.watch(key) { (scopedKey, result) =>
+         subscriber.onNext((scopedKey, result))
+      }
+      subscriber.add(Subscription {
+        subscription.cancel
+      })
+    }
+    observable
+  }
+  
 }
--- src/main/scala/org/skyluc/sbtrc/TestClient.scala
+++ src/main/scala/org/skyluc/sbtrc/TestClient.scala
@@ -9,14 +9,15 @@ import sbt.protocol._
 import sbt.client.SettingKey
 import sbt.client.TaskKey
 import sbt.Attributed
+import rx.lang.scala.Observer
 
 /** SbtServerLocator returning the bundled sbtLaunch.jar and sbt-server.properties. */
 class IDEServerLocator extends AbstractSbtServerLocator {
 
-  override def sbtLaunchJar: java.io.File = 
+  override def sbtLaunchJar: java.io.File =
     new File("/home/luc/.m2/repository/org/scala-sbt/sbt-launch/0.13.2-M2/sbt-launch-0.13.2-M2.jar")
 
-  override def sbtProperties(directory: java.io.File): java.net.URL = 
+  override def sbtProperties(directory: java.io.File): java.net.URL =
     new File("/home/luc/dev/scala-ide/sbt-remote-control/client/target/resource_managed/main/sbt-server.properties").toURI().toURL()
 
 }
@@ -30,65 +31,30 @@ object TestClient {
 class TestClient {
 
   def go() {
-    
+
     val root = new File("/home/luc/tmp/sbt/dummy-sbt-0.13")
-    val connector = new SimpleConnector(root , new IDEServerLocator())
+    val connector = new SimpleConnector(root, new IDEServerLocator())
 
-    connector.onConnect(newSbtClient(_, root))
+    connector.onConnect(newSbtClientObservable(_, root))
   }
-  
-  private def newSbtClient(sbtClient: SbtClient, root: File) {
+
+  private def newSbtClientObservable(sbtClient: SbtClient, root: File) {
     println(s"sbtClient: $sbtClient")
-    sbtClient.watchBuild(newBuild(_, sbtClient, root))
-  }
+    val client = new SbtClientWithObservableAndCache(sbtClient)
 
-  def newBuild(build: MinimalBuildStructure, sbtClient: SbtClient, root: File) = {
-    println(s"build: $build")
-    
-    val project = build.projects.head
-    
-    println(s"project: $project")
-    
-    watchBaseDirectory(project, sbtClient, root)
-    watchManagedClasspath(project, sbtClient, root)
-  }
-
-  def watchBaseDirectory(project: ProjectReference, sbtClient: SbtClient, root: File) = {
-    val aKey: AttributeKey = AttributeKey("baseDirectory", TypeInfo("java.io.File"))
-    val scope: SbtScope = SbtScope(Some(root.toURI()), Some(project), None, None)
-    val sKey: ScopedKey = ScopedKey(aKey, scope)
-    val key: SettingKey[File] = SettingKey(sKey)
-
-    println(s"key: $key")
-    
-    sbtClient.watch(key) { (a, b) =>
-      b match {
-        case TaskSuccess(value) =>
-          println(s">>>>>>${value.value.get}>>>>>${value.stringValue}")
-        case TaskFailure(msg) =>
-          println(s">>>>>>$msg")
-      }
+    for {
+      build <- client.buildValue
+      projectName = build.projects.head.name
+      baseDirectory <- client.getSettingValue[File](projectName, "baseDirectory", None)
+      managedClasspath <- client.getTaskValue[Seq[Attributed[File]]](projectName, "managedClasspath", Some("compile"))
+    } {
+      println(s"projectName: $projectName")
+      println(s"baseDirectory: $baseDirectory")
+      println(s"managedClasspath: $managedClasspath")
     }
-
-  }
-
-  def watchManagedClasspath(project: ProjectReference, sbtClient: SbtClient, root: File) = {
-    val aKey: AttributeKey = AttributeKey("managedClasspath", TypeInfo("scala.collection.Seq", Seq(TypeInfo("sbt.Attributed", Seq(TypeInfo("java.io.File"))))))
-    val scope: SbtScope = SbtScope(Some(root.toURI()), Some(project), Some("compile"), None)
-    val sKey: ScopedKey = ScopedKey(aKey, scope)
-    val key: TaskKey[Seq[Attributed[File]]] = TaskKey(sKey)
-
-    println(s"key: $key")
     
-    sbtClient.watch(key) { (a, b) =>
-      b match {
-        case TaskSuccess(value) =>
-          println(s">>>>>>${value.value.get}")
-        case TaskFailure(msg) =>
-          println(s">>>>>>$msg")
-      }
-    }
   }
-  
+
+
 }
 
